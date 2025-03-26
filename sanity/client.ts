9cce20a7380811a898d65b93b32c9305171bad50
Pap Ervin <73556420+Ervin-cyber@users.noreply.github.com>
2025-03-26 11:52:01 +0200

    sanity integrated successfully, querying content with GROQ and displaying detail content

---
+++ sanity/client.ts
@@ -0,0 +1,8 @@
+import { createClient } from "next-sanity";
+
+export const client = createClient({
+  projectId: "t2lmom4m",
+  dataset: "production",
+  apiVersion: "2024-01-01",
+  useCdn: false,
+});
---
+++ sanity/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: ' fatmobilityreport',
+
+  projectId: 't2lmom4m',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
