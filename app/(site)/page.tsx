73a077480c4e74e926002dd2684e31bab786e144
Pap Ervin <73556420+Ervin-cyber@users.noreply.github.com>
2025-04-02 10:52:58 +0300

    added revalidate option on main page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,7 @@
 import PostListComponent from "./components/PostListComponent";
 
+export const revalidate = 10
+
 export default async function IndexPage() {
   return (
     <PostListComponent filter="" slug={""} />
